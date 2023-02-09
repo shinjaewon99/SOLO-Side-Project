4f2aa7e20e9f6b9ed86d3a2849d440973389883a
shinjaewon99 <tlswodnjs99@naver.com>
2023-02-10 00:23:32 +0900

    feat : 초기 설정

---
+++ src/main/java/demoProject/shin/ShinApplication.java
@@ -0,0 +1,13 @@
+package demoProject.shin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demoProject/shin/ShinApplicationTests.java
@@ -0,0 +1,13 @@
+package demoProject.shin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
