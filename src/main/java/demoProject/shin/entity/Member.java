fa4ff38fd9743dd3c869135381f469e9bdd5ddb4
shinjaewon99 <tlswodnjs99@naver.com>
2023-02-18 00:13:56 +0900

    refactor : 초기설정 refactor

--- src/main/java/demoProject/shin/entity/Member.java
+++ src/main/java/demoProject/shin/entity/Member.java
@@ -1,23 +1,34 @@
 package demoProject.shin.entity;
 
 import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 @Getter
+/**
+ * @Builder 처럼 어떤 객체의 필드값을 편하게 저장하기 위해 사용한다. super가 붙었음으로 부모 객체를 상속받는 자식객체에서
+ * 부모 객체의 필드값도 저장할수 있게 하기 위해서 사용
+ */
+@SuperBuilder
+
 public class Member {
 
-    @Id
-    @GeneratedValue
-    private Long id;
+    @Column(unique = true)
+    private String username;  // 로그인 Id
+
+
 
     private String loginId;
-    private String name;
+
     private String mail;
     private String password;
+    private String nickName;
+
 
 
 }
--- src/main/java/demoProject/shin/repository/MemberRepository.java
+++ src/main/java/demoProject/shin/repository/MemberRepository.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
+// JpaRepository <T (객체), pk type>
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    // select m from Member m where m.name = ?  JPQL을 자동으로 생성 (스프링 데이터 Jpa를 사용했기때문)
     Optional<Member> findByName(String name);
 
     Optional<Member> findByEmail(String mail);
