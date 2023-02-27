66f70f44538bf61ae31a3cbcb156b95ba5d90527
shinjaewon99 <tlswodnjs99@naver.com>
2023-02-28 00:32:23 +0900

    feat : 엔티티 별로 중복된 코드를 제거하기위해 Main 클래스 어노테이션 추가

--- src/main/java/demoProject/shin/ShinApplication.java
+++ src/main/java/demoProject/shin/ShinApplication.java
@@ -2,8 +2,11 @@ package demoProject.shin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+// 모든 엔티티에 들어가는 공통 컬럼들을 공통Entity를 뽑아내서 사용할때 사용 (대표적으로, 등록일자 수정일자)
+@EnableJpaAuditing
 public class ShinApplication {
 
 	public static void main(String[] args) {
