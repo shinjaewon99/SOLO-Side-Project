ed9bb21ee88c5be99985e0972621e5931e9ddfb1
shinjaewon99 <tlswodnjs99@naver.com>
2023-03-06 21:31:20 +0900

    refactor : 회원가입 Test

--- src/test/java/demoProject/shin/controller/MemberServiceTest.java
+++ src/test/java/demoProject/shin/controller/MemberServiceTest.java
@@ -1,42 +1,63 @@
 package demoProject.shin.controller;
 
+import demoProject.shin.entity.Member;
 import demoProject.shin.service.MemberServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @WebMvcTest: Controller를 테스트하기 위한 어노테이션
  * Controller가 정상적으로 작동하는지 테스트하는 것이기 때문에 Web과 관련된 의존성만 가지고온다
+ * @WebMvcTest
  */
-@WebMvcTest
-class MemberControllerTest {
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
 
 
     /*
     MockMvc 란 ?
     개발한 웹 프로그램을 실제 서버에 배포하지 않고도 테스트를 위한 요청을 제공하는 수단 (흉내낸다 생각)
-    * */
     @Autowired
     MockMvc mockMvc;
-
     @MockBean
+    * */
+
+
     @Autowired
-    MemberServiceImpl memberService;
+    private MemberServiceImpl memberService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Test
     @DisplayName("회원가입 test")
     void join() {
         String username = "shin";
         String password = "12345";
+        String email = "shin@test.com";
+        String nickname = "hello";
+
+        memberService.login(username, password, email, nickname);
+        // Optional 타입은 get()으로 바로 꺼내면 안되지만, 테스트 코드임으로 get으로 바로 꺼냈다.
+        Member findMember = memberService.findByUsername("shin").get();
+
+        // Null 검증
+        assertThat(findMember.getUsername()).isNotNull();
+
+        /**
+         * 로그인 검증
+         * PasswordEncoder의 인터페이스 메서드(matches) 사용
+         */
+        assertThat(passwordEncoder.matches(password, findMember.getPassword())).isTrue();
+
+
     }
 
  /* MockMVC 테스트
